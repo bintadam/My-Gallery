7dea94bdf85ba65fdf5032f4fdedc80f42f3d4ef
zakiya adam <zakiyaadan12@gmail.com>
2022-03-22 22:19:13 +0300

    imported include function

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import re_path
+from django.urls import re_path
 from . import views
 
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
+    re_path('^today/$',views.pictures_of_day,name='picturesToday'),
 ]
