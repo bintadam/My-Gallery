a749fc8d1fd82d9575a78e538bcac6805b71cec5
zakiya adam <zakiyaadan12@gmail.com>
2022-03-24 19:41:09 +0300

    added __st__

--- pictures/models.py
+++ pictures/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField()
     image_name =models.CharField(max_length=70)
-    image_description = models.TextField()    
+    image_description = models.TextField()  
+
+    def __str__(self):
+      return self.first_name  
