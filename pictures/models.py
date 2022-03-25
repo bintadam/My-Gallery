9254d3d7901a6a0c7d1831dcf5864664ead8f464
zakiya adam <zakiyaadan12@gmail.com>
2022-03-25 14:15:49 +0300

    created models.foreignkey field for location

--- pictures/models.py
+++ pictures/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField()
     image_name =models.CharField(max_length=70)
-    image_description = models.TextField()  
+    image_description = models.TextField() 
+    location = models.ForeignKey() 
 
     def __str__(self):
       return self.first_name  
