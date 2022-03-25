bd0ff532a40b84841a284747ac2fb4adeb52bbf1
zakiya adam <zakiyaadan12@gmail.com>
2022-03-25 14:32:34 +0300

    created category model

--- pictures/models.py
+++ pictures/models.py
@@ -31,3 +31,15 @@ class Location(models.Model):
     def delete_location(self):
       self.save()   
     
+
+class Category(models.Model):
+    name = models.CharField(max_length =50)
+
+    def __str__(self):
+        return self.name 
+
+    def save_category(self):
+      self.save()  
+
+    def delete_category(self):
+      self.save()           
