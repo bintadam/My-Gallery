a4a3cac307db79d3800520b1fa1cedccde43c66e
zakiya adam <zakiyaadan12@gmail.com>
2022-03-25 14:26:06 +0300

    added save and delete function to location model

--- pictures/models.py
+++ pictures/models.py
@@ -19,7 +19,15 @@ class Image(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField(max_length =30)
+    name = models.CharField(max_length =50)
 
     def __str__(self):
         return self.name      
+
+    def save_location(self):
+      self.save()  
+
+
+    def delete_location(self):
+      self.save()   
+    
