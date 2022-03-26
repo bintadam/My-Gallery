fd552fd50f71f32b78b7bd330946d26084d90710
zakiya adam <zakiyaadan12@gmail.com>
2022-03-26 18:08:46 +0300

    added content to my welcome.html

--- pictures/models.py
+++ pictures/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField()
-    image_name =models.CharField(max_length=70)
+    image_name =models.CharField(max_length=50)
     image_description = models.TextField() 
-    location = models.ForeignKey() 
+    location = models.ForeignKey(Location, on_delete=models.CASCADE) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
       return self.first_name 
