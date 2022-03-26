1e4a896b73ae0621f917886950f16db22d62a8d0
zakiya adam <zakiyaadan12@gmail.com>
2022-03-26 21:42:50 +0300

    created class admin

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from . models import Image,Location,Category
 
 
 # Register your models here.
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ('Location','Category')
+
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
