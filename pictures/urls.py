b6458242fadf9e1c0ae6d6e054f22ecfd1dc9ab1
zakiya adam <zakiyaadan12@gmail.com>
2022-03-26 19:08:49 +0300

    created template files

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 from . import views
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
