07de71a3c811c921aed3863064785069a759fa8a
zakiya adam <zakiyaadan12@gmail.com>
2022-03-26 19:05:24 +0300

    imported project settings

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,7 +1,11 @@
 from django.urls import re_path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
     re_path('^pictures/$',views.pictures,name='pictures'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
