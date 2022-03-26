66a11b17a44280c670a7fff5979cf9c692624c06
zakiya adam <zakiyaadan12@gmail.com>
2022-03-26 19:36:44 +0300

    added function search_results

--- pictures/views.py
+++ pictures/views.py
@@ -24,3 +24,17 @@ def picture(request, past_date):
         
         raise Http404()
     return render (request, 'all-pictures/pictures.html', {"image": image}) 
+
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = pictures.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
